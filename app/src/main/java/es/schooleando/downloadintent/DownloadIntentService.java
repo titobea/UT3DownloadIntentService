423c2f6f824bcf2a6880d6909c371e1ec5cd1ada
Ruben Cancho <canchete@gmail.com>
2016-12-13 10:46:10 +0100

    Unos comentarios

--- app/src/main/java/es/schooleando/downloadintent/DownloadIntentService.java
+++ app/src/main/java/es/schooleando/downloadintent/DownloadIntentService.java
@@ -2,6 +2,7 @@ package es.schooleando.downloadintent;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.util.Log;
 
 
 /**
@@ -11,6 +12,13 @@ import android.content.Intent;
  * TODO: Customize class - update intent actions and extra parameters.
  */
 public class DownloadIntentService extends IntentService {
+    // definimos tipos de mensajes que utilizaremos en ResultReceiver
+    public static final int PROGRESS = 0;
+    public static final int FINSISHED = 1;
+    public static final int ERROR = 2;
+
+
+    private static final String TAG = "DownloadIntentService";
 
     public DownloadIntentService() {
         super("DownloadIntentService");
@@ -18,14 +26,25 @@ public class DownloadIntentService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        // Aquí deberás descargar el archivo y notificar a la Activity mediante un ResultReceiver
+
+        // Ejemplo de como logear
+        Log.d(TAG, "Servicio arrancado!");
+
+        // Aquí deberás descargar el archivo y notificar a la Activity mediante un ResultReceiver que recibirás en el intent.
+
+        // Deberamos obtener el ResultReceiver del intent
+        // intent.getParcelableExtra("receiver");
 
         // Es importante que definas el contenido de los Intent.
 
         // Por ejemplo:
-        //  - que enviarás al IntentService como parámetro inicial.
-        //  - que enviarás a ResultReceiver para notificarle incrementos en el porcentaje de descarga.
-        //  - que enviarás a ResultReceiver cuando se haya finalizado la descarga.
+        //  - que enviarás al IntentService como parámetro inicial (url a descargar)
+        //         intent.getgetStringExtra("url")
+        //  - que enviarás a ResultReceiver para notificarle incrementos en el porcentaje de descarga (número de 0 a 100)
+        //         receiver.send(PROGRESS, Bundle);
+        //  - que enviarás a ResultReceiver cuando se haya finalizado la descarga (nombre del archivo temporal)
+        //         receiver.send(FINISHED, Bundle);
+
 
 
 
--- app/src/main/java/es/schooleando/downloadintent/MainActivity.java
+++ app/src/main/java/es/schooleando/downloadintent/MainActivity.java
@@ -11,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Añade en el interfaz un botón y un TextView, como mínimo.
+
+        // cuando pulsemos el botón deberemos crear un Intent que contendrá un Bundle con:
+        // una clave "url" con la dirección de descarga asociada.
+        // una clave "receiver" con un objeto ResultReceiver.
+        //
+        // El objeto ResultReceiver contendrá el callback que utilizaremos para recibir
+        // mensajes del IntentService.
+
+        // después deberás llamar al servicio con el intent.
     }
 }
