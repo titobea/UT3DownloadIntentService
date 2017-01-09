d35981e2e1301268c8e0db17f32068e959dd212c
titobea <tito_bea@hotmail.com>
2017-01-09 15:42:29 +0100

    Add files via upload

--- app/src/main/java/es/schooleando/downloadintent/DownloadIntentService.java
+++ app/src/main/java/es/schooleando/downloadintent/DownloadIntentService.java
@@ -1,8 +1,21 @@
 package es.schooleando.downloadintent;
 
 import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.os.ResultReceiver;
 import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 /**
@@ -14,7 +27,7 @@ import android.util.Log;
 public class DownloadIntentService extends IntentService {
     // definimos tipos de mensajes que utilizaremos en ResultReceiver
     public static final int PROGRESS = 0;
-    public static final int FINSISHED = 1;
+    public static final int FINISHED = 1;
     public static final int ERROR = 2;
 
 
@@ -29,23 +42,83 @@ public class DownloadIntentService extends IntentService {
 
         // Ejemplo de como logear
         Log.d(TAG, "Servicio arrancado!");
-
         // Aquí deberás descargar el archivo y notificar a la Activity mediante un ResultReceiver que recibirás en el intent.
-
-        // Deberamos obtener el ResultReceiver del intent
-        // intent.getParcelableExtra("receiver");
-
+        // Deberemos obtener el ResultReceiver del intent
+        ResultReceiver receiver= intent.getParcelableExtra("receiver");
+        String ruta = intent.getStringExtra("url");
         // Es importante que definas el contenido de los Intent.
-
-        // Por ejemplo:
         //  - que enviarás al IntentService como parámetro inicial (url a descargar)
-        //         intent.getgetStringExtra("url")
+        //String url= intent.getStringExtra("url");
+        //intent.putExtra("aaa", "aaa");
+        //Bundle parameters = intent.getExtras();
+        Bundle b = new Bundle();
+        //b.putInt("progreso",3);
         //  - que enviarás a ResultReceiver para notificarle incrementos en el porcentaje de descarga (número de 0 a 100)
-        //         receiver.send(PROGRESS, Bundle);
+        //receiver.send(PROGRESS, b);
         //  - que enviarás a ResultReceiver cuando se haya finalizado la descarga (nombre del archivo temporal)
-        //         receiver.send(FINISHED, Bundle);
+        //receiver.send(FINISHED, b);
+        //receiver.send(ERROR, b);
+
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo ni = cm.getActiveNetworkInfo();
+        if (ni != null && ni.isConnected()) {
+            URL url= null;
+            try {
+                //ruta="http://tresbits.es/miwordpress/wp-content/uploads/2015/06/tresbits2.png";
+                url = new URL(ruta);
+
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setRequestMethod("HEAD");
+                con.connect();
+
+                int size = con.getContentLength();
+
+                InputStream in = url.openStream();
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+                byte[] by = new byte[1024];
+
+                for (int i; (i = in.read(by)) != -1; ) {
+
+                    out.write(by, 0, i);
+                    if (size>0) {
+                        b.putInt("progreso",out.size() * 100 / size);
+                    }else{
+                        b.putInt("progreso",i*-1);
+                    }
+                    receiver.send(PROGRESS, b);
+                }
 
 
+                String[] data= ruta.split("/");
+                String[] f= data[data.length-1].split("\\.");
+
+                if (f.length<2) f= new String[]{"unknown","jpg"};
+
+                File outputDir = getExternalCacheDir();//getCacheDir();
+
+                File outputFile = File.createTempFile(f[0],"."+ f[1], outputDir);
+                outputFile.deleteOnExit();
+
+                FileOutputStream fos = new FileOutputStream(outputFile);
+                fos.write(out.toByteArray());
+
+                b.putString("ruta",outputFile.getPath());
+                receiver.send(FINISHED, b);
+
+                out.close();
+                in.close();
+
+
+            } catch (Exception e) {
+                b.putString("mensaje","URL incorrecta "+e.getMessage());
+                receiver.send(ERROR, b);
+            }
+
+        } else {
+            b.putString("mensaje","No hay conexión");
+            receiver.send(ERROR, b);
+        }
 
 
     }
--- app/src/main/java/es/schooleando/downloadintent/MainActivity.java
+++ app/src/main/java/es/schooleando/downloadintent/MainActivity.java
@@ -1,14 +1,43 @@
 package es.schooleando.downloadintent;
 
+import android.app.IntentService;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Handler;
+import android.os.ResultReceiver;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+
+import static java.security.AccessController.getContext;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar pb;
+    private TextView tv;
+    private EditText txtUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //recoger variables del layout
+        pb = (ProgressBar)findViewById(R.id.progressBar);
+        pb.setVisibility(View.INVISIBLE);
+        tv = (TextView)findViewById(R.id.barPercent);
+        txtUrl =(EditText) findViewById(R.id.txtUrl);
+
 
         // Añade en el interfaz un botón y un TextView, como mínimo.
 
@@ -20,5 +49,72 @@ public class MainActivity extends AppCompatActivity {
         // mensajes del IntentService.
 
         // después deberás llamar al servicio con el intent.
+    }
+
+    public void descargarUrl(View v){
+        if (!txtUrl.getText().toString().trim().equals("")){
+            pb.setVisibility(View.VISIBLE);
+            Intent i = new Intent(this,DownloadIntentService.class);
+            i.putExtra("url",txtUrl.getText().toString());
+            ResultReceiver receiver = new ResultReceiver(new Handler()) {
+                protected void onReceiveResult(int resultCode, Bundle resultData) {
+                    //procesar resultados en UI Thread
+                    switch (resultCode) {
+                        case DownloadIntentService.PROGRESS:
+                            int progreso = resultData.getInt("progreso");
+                            pb.setIndeterminate(progreso < 0);
+                            if (progreso > 0) tv.setText("" + progreso + "%");
+                            pb.setProgress(progreso);
+                            break;
+                        case DownloadIntentService.FINISHED:
+                            String ruta = resultData.getString("ruta");
+                            pb.setVisibility(View.INVISIBLE);
+                            tv.setText("");
+                            File f =new File(ruta);
+                            if (f.exists()) {
+
+                                MimeTypeMap mime = MimeTypeMap.getSingleton();
+                                //String ext = f.getName().substring(f.getName().lastIndexOf(".") + 1);
+                                String ext = android.webkit.MimeTypeMap.getFileExtensionFromUrl(Uri.fromFile(f).toString());
+                                String type = mime.getMimeTypeFromExtension(ext);
+                                try {
+                                    Intent intent = new Intent();
+                                    intent.setAction(Intent.ACTION_VIEW);
+                                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                        intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                                        Uri contentUri = FileProvider.getUriForFile(MainActivity.this, "com.your.package.fileProvider", f);
+                                        intent.setDataAndType(contentUri, type);
+                                    } else {
+                                        intent.setDataAndType(Uri.fromFile(f), type);
+                                    }
+                                    startActivityForResult(intent,0);
+                                } catch (ActivityNotFoundException anfe) {
+                                    Toast.makeText(MainActivity.this, "No activity found to open this attachment.", Toast.LENGTH_LONG).show();
+                                }
+                            }else {
+                                Toast.makeText(MainActivity.this,"No existe el fichero descargado en " + ruta,Toast.LENGTH_SHORT).show();
+                            }
+
+
+
+                                /*Intent i = new Intent(Intent.ACTION_VIEW, Uri.fromFile(f));
+                                i.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                                startActivity(i);*/
+
+                            break;
+                        case DownloadIntentService.ERROR:
+                            String mensaje = resultData.getString("mensaje");
+                            pb.setVisibility(View.INVISIBLE);
+                            tv.setText("");
+                            Toast.makeText(MainActivity.this,mensaje,Toast.LENGTH_SHORT).show();
+                            break;
+                    }
+                }
+            };
+            i.putExtra("receiver",receiver);
+            startService(i);
+        }else {
+            Toast.makeText(this,"Escriba una Url",Toast.LENGTH_SHORT).show();
+        }
     }
 }
